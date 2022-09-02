67cecf622ab0f6b8beb9ddea28cb3046754f631f
joaomouraosa <joao.mourao.sa@gmail.com>
2022-09-02 15:35:42 +0100

    allow it to run with arguments

--- coomer.party_scraper.py
+++ coomer.party_scraper.py
@@ -1,132 +1,160 @@
 from bs4 import BeautifulSoup
-from more_itertools import last
-import requests 
+import requests
 import time
 import os
+import os.path
+from os import path
+import sys
+from more_itertools import last
 
-from sqlalchemy import null
-    
-scrapeVideos = False
+ENTRIES_FILE = "entries.tmp"
+DOWNLOAD_VIDEOS = True
 
-def coom():
-    number = 0
-    file = open("Entry List.txt", "r+").readlines()
-    #For Every Entry
+
+def coom(file_path):
+
+    file = open(ENTRIES_FILE, "r+").readlines()
+
     for entry in file:
-        print("\nNew Entry Started: "+entry+"\n")
-        #open page of entry
-        result = requests.get("https://coomer.party"+str(entry).strip())
-        postpage = BeautifulSoup(result.content, "html.parser")
-        #find the img links
-        try:
-            postfiles = postpage.find("div", class_="post__files")
+
+        images, videos = [], []
+
+        post = entry.split("\n")[0].split("/")[-1]
+
+        print("== Post: " + post)
+
+        result = requests.get("https://coomer.party" + str(entry).strip())
+        postPage = BeautifulSoup(result.content, "html.parser")
+
+        try :
+            postfiles = postPage.find("div", class_="post__files")
             images = postfiles.find_all("a")
         except:
-            print("\nNo images found in this entry\n")
+            pass
 
-        #if get videos is selected, then also find video links
-        if scrapeVideos == True:
+        if DOWNLOAD_VIDEOS:
             try:
-                entryattachments = postpage.find("ul", class_="post__attachments")
-                videos = entryattachments.find_all("a")
+                entryAttachments = postPage.find("ul", class_="post__attachments")
+                videos = entryAttachments.find_all("a")
 
-                for video in videos:
+                for n, video in enumerate(videos):
+
+                    path_file = f'{file_path}/{post}_{str(n)}.mp4'
+                    if path.exists(path_file):
+                        print("File exists. continue")
+                        continue
+
                     try:
                         video_src = video["href"]
-                        print(video_src)
+                        print(f'Downloading video {video_src}')
                     except:
-                        print("\nError in ripping a video\n")
+                        print("Error in ripping a video")
                         continue
-                    video_data = requests.get("https://data1.coomer.party"+video_src).content
-                    with open(str(number)+'.mp4', 'wb') as handler:
+
+                    video_data = requests.get("https://data1.coomer.party" + video_src).content
+                    with open(path_file, 'wb') as handler:
                         handler.write(video_data)
-                    number += 1
-
             except:
-                print("No videos for entry")
+                pass
 
+        # save images to disk
+        for n, image in enumerate(images):
 
-        #save images to disk
-        for image in images:
+            path_file = f'{file_path}/{post}_{str(n)}.jpg'
+            if path.exists(path_file):
+                print("Image exists. continue")
+                continue
+
             try:
                 image_src = image["href"]
-                print(image_src)
+                print(f'Downloading image {image_src}')
             except:
-                print("Error in ripping an image\n")
                 continue
-            img_data = requests.get("https://www.coomer.party"+image_src).content
-            with open(f'{file_path}/{str(number)}.jpg', 'wb') as handler:
+            img_data = requests.get("https://www.coomer.party" + image_src).content
+            with open(path_file, 'wb') as handler:
                 handler.write(img_data)
-            number += 1
-    print("Cooming has finished, enjoy your files!")
+    print("End")
 
-def ScanningPosts(url):
-    try:
-        result = requests.get(url)
-        mainpage = BeautifulSoup(result.content, "html.parser")
-    except:
-        print("URL brought an error, please check and make sure it is formatted correctly\neg. https://www.coomer.party + creator link with no extra characters or page identifiers")
-    posts = []
 
-#calculate amount of pages
+def _get_pages_number(page):
     try:
-        nextpagebutton = mainpage.find(title="Next page").parent
-        maxpagelink = nextpagebutton.find_previous("li")
-        lin = maxpagelink.find("a")
-        link = lin["href"]
+        nextPageBtn = page.find(title="Next page").parent
+        maxPageLink = nextPageBtn.find_previous("li")
+        link = maxPageLink.find("a")["href"]
         lastPage = str(link).split("=")[-1]
-        print("Successfully Calculated Amount of Pages as "+str((int(lastPage)/25)+1))
+        lastPage = int(lastPage) / 25 + 1
     except:
         lastPage = 1
-        print("Successfully Calculated Amount of Pages as 1")
+    return lastPage
 
-    #Finds all Entry links on current page
+def ScanningPosts(handle):
+
+    URL = f'https://coomer.party/onlyfans/user/{handle}'
+    try:
+        result = requests.get(URL)
+        mainPage = BeautifulSoup(result.content, "html.parser")
+    except:
+        print("URL error. Make sure it is formatted correctly")
+
+    posts = []
+
+    lastPage = _get_pages_number(mainPage)
+
+    print("PAGES #" + str(lastPage))
+
+    # Finds all Entry links on current page
     def fetchPagesEntryLinks():
-        #find the entry link html code
-        tagOfPosts = mainpage.find_all("h2",class_="post-card__heading")
+        # find the entry link html code
+        tagOfPosts = mainPage.find_all("h2", class_="post-card__heading")
         for post_ in tagOfPosts:
             link = post_.find("a")
-            #add said entry link to posts list
+            # add said entry link to posts list
             posts.append(link["href"])
-            print("Discovered entry: "+str(link["href"]))
+            print("Discovered entry: " + str(link["href"]))
 
-    #fetch first page links then iterate through pages until last page
-    pageNumber = 0
-    while pageNumber <= int(lastPage):
+    # fetch first page links then iterate through pages until last page
+
+    for pageNumber in range(0, int(lastPage)):
         fetchPagesEntryLinks()
         pageNumber += 25
-        print("Fetching posts on page "+str((pageNumber/25)))
+        print("Fetching posts on page " + str((pageNumber / 25)))
         time.sleep(1)
-        result = requests.get(url+"?o="+str(pageNumber))
-        mainpage = BeautifulSoup(result.content, "html.parser")
+        result = requests.get(f'{URL}?o={str(pageNumber)}')
+        print(result)
+        mainPage = BeautifulSoup(result.content, "html.parser")
 
-    postlistFile = open("Entry List.txt", "w")
+    postlistFile = open(ENTRIES_FILE, "w")
     for entry in posts:
-        postlistFile.write(entry+"\n")
+        postlistFile.write(entry + "\n")
     postlistFile.close()
-    continuetocoom = input("\nFinished gathering entries, begin scraping?\nY / N: ").lower().strip()
-    if continuetocoom == "y":
-        coom()
-    else:
-        print("Exiting...")
-        exit
-#START
-print("\nWelcome to the coomer.party scraper made by EGirlEnthusiast")
-try:
-    testtoseeiffileexists = open("Entry List.txt", "r")
-    print("\nEntries detected, skipping to rip, to recalculate posts delete Entry List.txt\n")
-    includevidschoice = input("Include scraping of videos?\nY / N: ").lower().strip()
-    if includevidschoice == "y":
-        scrapeVideos = True
-    else:
-        scrapeVideos = False
-    coom()
-except:
-    url = input("Please Enter URL of creator from coomper.party (Ensure no text exists after the creator's name, in the link)\nURL: ")
-    file_path = input('Please Enter the FILE PATH where you want to save the elements\nFILE PATH: ')
+
+
+#def main(handle: str, output_dir: str):
+def main():
+
+    # url = input("Please Enter the URL of the creator from coomer.party: ")
+
+    print("Usage: ")
+    print("$ python Coomer.party-scraper [handle] [output-dir]")
+    print("eg: python Coomer.party-scraper belledelphine ~/Images/")
+
+    if (len(sys.argv) != 3):
+        exit()
+
+    handle, output_dir = sys.argv[1], sys.argv[2]
+
+    if (output_dir[-1]=='/'):
+        output_dir=output_dir[0:-1]
+
+    file_path = f'{output_dir}/{handle}'
     if os.path.isdir(file_path):
         pass
     else:
         os.mkdir(file_path)
-    ScanningPosts(url)
+    ScanningPosts(handle)
+    coom(file_path)
 
+
+if __name__ == "__main__":
+    main()
+    #typer.run(main)
